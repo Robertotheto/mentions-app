63f939a5c23164639dc963dda6210d0f8da8132b
Roberto dos Santos Filho <robertodossantosfilho1001@gmail.com>
2020-04-01 08:57:24 -0400

    fazendo a conexão da aplicação com nosso banco de dados

--- src/app.js
+++ src/app.js
@@ -1,6 +1,31 @@
 const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
 //App
 const app = express()
+//Database
+mongoose.connect(process.env.DATABASE_CONNECTION_STRING,{
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+    useNewUrlParser: true,
+    useCreateIndex: true 
+})
+const db = mongoose.connection
+db.on('connected',() => {
+    console.log('Mongoose default connection is open')
+})
+db.on('error', err =>{
+    console.log(`Mongoose default connection has occured \n ${err}`)
+})
+db.on('disconnected',() => {
+    console.log('Mongoose default connection is disconnected')
+})
+process.on('SIGINT',() => {
+    console.log(
+        'Mongoose default connection is disconnected due to application termination'
+    )
+    process.exit(0)
+})
 //Load Router
 const indexRoutes = require('./routes/index-routes')
 app.use('/' ,indexRoutes)
